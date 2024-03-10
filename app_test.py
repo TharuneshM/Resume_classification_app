05b372c9572b6620474d693acf0fbfdc56486e01
TharuneshM <119936370+TharuneshM@users.noreply.github.com>
2024-03-10 14:59:47 +0530

    Update app_test.py

--- app_test.py
+++ app_test.py
@@ -1,25 +1,30 @@
-from flask import Flask, request, render_template
+import streamlit as st
+import pandas as pd
 import re
 import nltk
 import joblib
 import numpy as np
-import os
-import string
+from sklearn.svm import SVC
+from sklearn.feature_extraction.text import TfidfVectorizer
 import aspose.words as aw
+import math
+from wordcloud import WordCloud, STOPWORDS
+import string
 from nltk.corpus import stopwords
-from werkzeug.utils import secure_filename
 import matplotlib.pyplot as plt
 
-# Initialize Flask app
-app = Flask(__name__)
-
 # Load pre-trained TfidfVectorizer and SVM model
-current_dir = os.path.dirname(__file__)
-word_vectorizer = joblib.load(os.path.join(current_dir, 'word_vectorizer.pkl'), 'rb')
-clf_pkl = joblib.load(os.path.join(current_dir, 'fmodel.sav'), 'rb')
+word_vectorizer = joblib.load(open('C:\\Users\\Tharun\\Desktop\\PROJECT ML\\resume classification\\word_vectorizer.pkl', 'rb'))
+clf_pkl = joblib.load(open('C:\\Users\\Tharun\\Desktop\\PROJECT ML\\resume classification\\fmodel.sav','rb'))
 
 # Define category mapping
 category_mapping = {0: "PeopleSoft Resume", 1: "React JS Developer Resume", 2: "SQL Developer Lightning Insight Resume", 3: "Workday Resume"}
+ 
+# Define Streamlit app
+st.title('Resume Classifier')
+
+# Upload resume file
+uploaded_file = st.file_uploader('Upload a resume PDF file:', type=['pdf','doc','docx','docs'])
 
 # Preprocessing function
 def preprocess(txt):
@@ -33,29 +38,22 @@ def preprocess(txt):
     txt = nltk.tokenize.word_tokenize(txt)
     txt = [w for w in txt if not w in nltk.corpus.stopwords.words('english')]
     return ' '.join(txt)
-
+    
 def preprocess_and_transform(text):
     preprocessed_text = preprocess(text)
     tfidf_vector = word_vectorizer.transform([preprocessed_text])
     return tfidf_vector
-
-@app.route('/')
-def home():
-    return render_template('index.html')
-
-@app.route('/predict', methods=['POST'])
-def predict():
-    uploaded_file = request.files['file']
-    filename = secure_filename(uploaded_file.filename)
-    file_path = os.path.join(os.path.join(current_dir,'store'), filename)
-    uploaded_file.save(file_path)
-
-    # Read file using Aspose Words
-    doc = aw.Document(file_path)
+    
+# Process and predict on file
+if uploaded_file is not None:
+    # Read file using Apose Words
+    doc = aw.Document(uploaded_file)
     doc_text = doc.get_text().strip()
 
     # Preprocess text
     doc_text_processed = preprocess(doc_text)
+
+    # Create a frequency distribution of words
     oneSetOfStopWords = set(stopwords.words('english')+['``',"''"])
     totalWords = []
     for word in nltk.word_tokenize(doc_text_processed):
@@ -68,23 +66,43 @@ def predict():
      # Create a bar plot of the most common words
     words = [x[0] for x in mostcommon]
     freqs = [x[1] for x in mostcommon]
-    plt.bar(words, freqs)
-    plt.xlabel('Words')
-    plt.ylabel('Frequency')
-    plt.title('Most Common Words')
-    plt.xticks(rotation=90)
-    plt.tight_layout()
-    plt.savefig('static/word_frequency_plot.png')
-    plt.close()  # Close the plot to free memory
+    fig, ax = plt.subplots()
+    ax.bar(words, freqs)
+    ax.set_xticklabels(words, rotation=90)
+    ax.set_xlabel('Words')
+    ax.set_ylabel('Frequency')
+    ax.set_title('Most Common Words')
+    st.pyplot(fig)
+
+    
+    # Generate word cloud
+    wordcloud = WordCloud(stopwords=STOPWORDS,
+                          background_color='black',
+                          width=1200,
+                          height=1200
+                         ).generate(doc_text_processed)
+
+    # Display the word cloud
+    st.image(wordcloud.to_array(), use_column_width=True)
 
     # Preprocess and transform text
     tfidf_vector = preprocess_and_transform(doc_text)
 
     # Predict category using the pre-trained model
     category_id = clf_pkl.predict(tfidf_vector)[0]
+
+     # Predict probability percentage
+    probab= clf_pkl.predict_proba(tfidf_vector)
+
+    #st.write(f"Probability : {probab.values}")
+    st.write(f"<span style='font-size:30px;'>Probability : {np.round(probab.max(),3)*100} % </span>", unsafe_allow_html=True)
+
+    #st.write(type(probab))
     category = category_mapping[category_id]
 
-    return render_template('result.html', category=category)
+    # Display predicted domain category
+    st.write(f"<span style='font-size:30px;'>Predicted Category: {category}</span>", unsafe_allow_html=True)
 
-if __name__ == "__main__":
-    app.run(debug=True)
+    #st.write(f'Predicted Category: {category}')
+
+#   streamlit run "C:\Users\Hi\Desktop\DS_Projects\resume\resume_app.py"
